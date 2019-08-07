72c604fc81b063495f9016f26a723ff2556e0242
Santtu Lempinen <santtu.lempinen@helsinki.fi>
2019-08-07 15:45:53 +0300

    supresses warnings

--- libstomper/src/huffman.rs
+++ libstomper/src/huffman.rs
@@ -5,11 +5,13 @@ pub struct Huffman;
 
 impl super::Compressor for Huffman {
     /// Not yet implemented
+    #[allow(unused_variables)]
     fn encode(input: &mut impl Read, output: &mut impl Write) -> Result<(), Box<dyn Error>> {
         Ok(())
     }
 
     /// Not yet implemented
+    #[allow(unused_variables)]
     fn decode(input: &mut impl Read, output: &mut impl Write) -> Result<(), Box<dyn Error>> {
         Ok(())
     }
