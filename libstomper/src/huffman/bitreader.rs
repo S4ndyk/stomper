4bf666cbf887e5ef53e4249dfe5a3014c3283381
Santtu Lempinen <santtu.lempinen@helsinki.fi>
2019-08-30 18:52:17 +0300

    bitreader and writer added

---
+++ libstomper/src/huffman/bitreader.rs
@@ -0,0 +1,114 @@
+use std::io::Read;
+
+const BUFFER_SIZE: usize = 128;
+
+pub struct BitReader<R: Read> {
+    reader: R,
+    buffer: [u8; BUFFER_SIZE],
+    end: usize,
+    bytecount: usize,
+    bitpos: usize,
+}
+
+impl <R: Read> BitReader<R> {
+    fn new(reader: R) -> Self {
+        let mut bitreader = BitReader {
+            reader,
+            buffer: [0; BUFFER_SIZE],
+            end: 0,
+            bytecount: 0,
+            bitpos: 0,
+        };
+        bitreader.read_to_buffer();
+        bitreader
+    } 
+
+    fn next_bit(&mut self) -> Option<bool> {
+        if self.bytecount > BUFFER_SIZE - 1 {
+            self.read_to_buffer();
+        }
+
+        if self.bytecount == self.end {
+            return None
+        };
+
+        let next = self.buffer[self.bytecount];
+        let mask = 1 << self.bitpos;
+        self.bitpos += 1;
+        if self.bitpos > 7 {
+            self.bytecount += 1;
+            self.bitpos = 0;
+        }
+        if next & mask != 0 {
+            Some(true)
+        } else {
+            Some(false)
+        }
+    }    
+
+    fn read_to_buffer(&mut self) -> usize {
+        self.end = self.reader.read(&mut self.buffer).expect("Could not read to buffer");
+        self.bytecount = 0;
+        self.bitpos = 0;
+        self.end
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::BitReader;
+    #[test]
+    fn reader_reads_correctly_no1() {
+        let mut v = Vec::new();
+        v.push(0b10110011);
+        let mut reader = BitReader::new(v.as_slice());
+        assert_eq!(reader.next_bit(), Some(true));
+        assert_eq!(reader.next_bit(), Some(true));
+        assert_eq!(reader.next_bit(), Some(false));
+        assert_eq!(reader.next_bit(), Some(false));
+        assert_eq!(reader.next_bit(), Some(true));
+        assert_eq!(reader.next_bit(), Some(true));
+        assert_eq!(reader.next_bit(), Some(false));
+        assert_eq!(reader.next_bit(), Some(true));
+        assert_eq!(reader.next_bit(), None);
+    }
+
+    #[test]
+    fn reader_reads_correctly_no2() {
+        let mut v = Vec::new();
+        v.push(0b10110011);
+        v.push(0b11110000);
+        v.push(0b00110011);
+        let mut reader = BitReader::new(v.as_slice());
+        assert_eq!(reader.next_bit(), Some(true));
+        assert_eq!(reader.next_bit(), Some(true));
+        assert_eq!(reader.next_bit(), Some(false));
+        assert_eq!(reader.next_bit(), Some(false));
+        assert_eq!(reader.next_bit(), Some(true));
+        assert_eq!(reader.next_bit(), Some(true));
+        assert_eq!(reader.next_bit(), Some(false));
+        assert_eq!(reader.next_bit(), Some(true));
+        
+
+        assert_eq!(reader.next_bit(), Some(false));
+        assert_eq!(reader.next_bit(), Some(false));
+        assert_eq!(reader.next_bit(), Some(false));
+        assert_eq!(reader.next_bit(), Some(false));
+        assert_eq!(reader.next_bit(), Some(true));
+        assert_eq!(reader.next_bit(), Some(true));
+        assert_eq!(reader.next_bit(), Some(true));
+        assert_eq!(reader.next_bit(), Some(true));
+        
+        
+        assert_eq!(reader.next_bit(), Some(true));
+        assert_eq!(reader.next_bit(), Some(true));
+        assert_eq!(reader.next_bit(), Some(false));
+        assert_eq!(reader.next_bit(), Some(false));
+        assert_eq!(reader.next_bit(), Some(true));
+        assert_eq!(reader.next_bit(), Some(true));
+        assert_eq!(reader.next_bit(), Some(false));
+        assert_eq!(reader.next_bit(), Some(false));
+
+        assert_eq!(reader.next_bit(), None) 
+    }
+}
---
+++ libstomper/src/huffman/bitwriter.rs
@@ -0,0 +1,119 @@
+use std::io::Write;
+
+const BUFFER_SIZE: usize = 128;
+
+pub struct BitWriter<W: Write>{
+    writer: W,
+    buffer: [u8; BUFFER_SIZE],
+    bytecount: usize,
+    bitpos: usize,
+}
+
+impl <W: Write> BitWriter<W> {
+    fn new(writer: W) -> Self {
+        BitWriter {
+            writer,
+            buffer: [0; BUFFER_SIZE],
+            bytecount: 0,
+            bitpos: 0,
+        }
+    } 
+
+    fn write_bit(&mut self, set_bit: bool) {
+        if set_bit {
+            let mask = 1 << self.bitpos;
+            let byte = self.buffer[self.bytecount];
+            self.buffer[self.bytecount] = byte | mask;
+        }
+        self.bitpos += 1;
+        if self.bitpos > 7 {
+            self.bitpos = 0;
+            self.bytecount += 1;
+        }
+
+        if self.bytecount > BUFFER_SIZE - 1 {
+            self.flush();
+        }
+    } 
+
+    fn flush(&mut self) {
+        self.writer.write_all(&self.buffer).expect("Could not write to all to writer");
+        self.bitpos = 0;
+        self.bytecount = 0;
+        self.buffer = [0; BUFFER_SIZE];
+    }
+
+    pub fn write_string(&mut self, s: String) {
+        for c in s.bytes() {
+            if c as char == '1' {
+                self.write_bit(true);
+            }
+            if c as char == '0' {
+                self.write_bit(false);
+            }
+        }
+    }
+
+    /// Flushes and rerturns inner writer 
+    fn to_inner(mut self) -> W {
+        self.flush();
+        self.writer
+    }
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::BitWriter;
+
+    #[test]
+    fn writer_writes_correctly_no1() {
+        let s = String::from("100101");
+        let buf: Vec<u8> = Vec::new();
+        let mut writer = BitWriter::new(buf);
+        writer.write_string(s);
+        let buf = writer.to_inner();
+        assert_eq!(buf[0], 0b101001);
+    }
+
+    #[test]
+    fn writer_writes_correctly_no2() {
+        let s = String::from("111001011101");
+        let buf: Vec<u8> = Vec::new();
+        let mut writer = BitWriter::new(buf);
+        writer.write_string(s);
+        let buf = writer.to_inner();
+        assert_eq!(buf[0], 0b10100111);
+        assert_eq!(buf[1], 0b1011);
+    }
+
+    #[test]
+    fn writer_writes_correctly_no3() {
+        let s1 = String::from("1110");
+        let s2 = String::from("01");
+        let s3 = String::from("01110");
+        let s4 = String::from("1");
+        let buf: Vec<u8> = Vec::new();
+        let mut writer = BitWriter::new(buf);
+        writer.write_string(s1);
+        writer.write_string(s2);
+        writer.write_string(s3);
+        writer.write_string(s4);
+        let buf = writer.to_inner();
+        assert_eq!(buf[0], 0b10100111);
+        assert_eq!(buf[1], 0b1011);
+    }
+
+    #[test]
+    fn writer_does_not_overflow() {
+        let buf: Vec<u8> = Vec::new();
+        let mut writer = BitWriter::new(buf);
+        for i in 0..3072 {
+            if i%3 == 0 {
+                writer.write_bit(false);
+            } else {
+                writer.write_bit(true);
+            }
+        }
+    }
+}
